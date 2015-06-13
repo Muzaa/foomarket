476e1769e6e743f7f14a0e2e6020d9b698983274
Muzaa <muzhafarahzan@Muza-MacBook.local>
2015-06-13 22:10:54 +0800

    add function for inventory

---
+++ v3/add.php
@@ -0,0 +1,37 @@
+<div class="pages">
+  <div data-page="projects" class="page no-toolbar no-navbar">
+    <div class="page-content">
+    
+     <div class="navbarpages">
+       <div class="navbar_home_link"><a href="index.html"><img src="images/icons/white/home.png" alt="" title="" /></a></div>
+       <div class="navbar_page_center">Contact</div>
+       <div class="menu_open_icon_white"><a href="#" class="open-panel"><img src="images/menu_open.png" alt="" title="" /></a></div>
+     </div>
+     <div id="pages_maincontent">
+      
+      <h2 class="page_subtitle">Insert new item to inventory</h2>
+
+            <h2 id="Note"></h2>
+            <div class="contactform">
+            <form class="cmxform" id="ContactForm" method="post" action="">
+            <label>Description:</label>
+            <input type="text" name="ContactName" id="ContactName" value="" class="form_input required" />
+            <label>Rate per hour:</label>
+            <input type="text" name="ContactEmail" id="ContactEmail" value="" class="form_input required email" />
+            <label>Deposit (if any):</label>
+            <textarea name="ContactComment" id="ContactComment" class="form_textarea textarea required" rows="" cols=""></textarea>
+            <input type="submit" name="submit" class="form_submit" id="submit" value="Send" />
+            <label id="loader" style="display:none;"><img src="images/loader.gif" alt="Loading..." id="LoadingGraphic" /></label>
+            </form>
+            </div>
+            
+
+      <div class="call_button"><a href="index.html">Back to Home</a></div>    
+      
+      <div class="clear"></div>
+      </div>
+      
+      
+    </div>
+  </div>
+</div>
