de5ac29a2f520a7c69ffbfb4278f18a40b0f4964
Muzaa <muzhafarahzan@Muza-MacBook.local>
2015-06-14 02:56:28 +0800

    update the add part

--- v3/addsuccess.php
+++ v3/addsuccess.php
@@ -1,3 +1,6 @@
+<?php
+include "config.php";
+?>
 <div class="pages">
   <div data-page="projects" class="page no-toolbar no-navbar">
     <div class="page-content">
@@ -10,13 +13,41 @@
      <div id="pages_maincontent">
       
       <h2 class="page_subtitle">Thank you, your item is inserted into our database.</h2>
+<div class="clear"></div>
 
-         
-            
+<h5>
+     <?php
+     
+     
+     $qid = mysql_query("select * from inventory a , user b where a.userid = b.uid AND iid = $_GET[id]");
 
-      <div class="call_button"><a href="index.php">Back to Home</a></div>    
-      
+	      if($data = mysql_fetch_array($qid))
+	      {
+              //  var_dump($data);
+                
+                echo "<img src='uploads/$data[iid].jpg'> <Br>
+                
+              Item Description :  $data[Name]
+              <br>
+
+Rate : $data[Rate] USD
+<br>
+
+Deposit : $data[Deposit] USD
+
+";
+                
+                
+              }
+     ?>
+</h5>
+
+     
+      <br>
       <div class="clear"></div>
+      <br>
+        
+          <div class="call_button"><a href="index.php">Back to Home</a></div>   
       </div>
       
       
